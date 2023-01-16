88153bd3b3d81153bae15f636a1161e7a6f01fd4
minyoung.c <devcmy136@gmail.com>
2023-01-16 16:36:09 +0900

    **

--- servletSite/src/main/java/com/itwill/servlet/GugudanTextServlet.java
+++ servletSite/src/main/java/com/itwill/servlet/GugudanTextServlet.java
@@ -14,13 +14,14 @@ public class GugudanTextServlet extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-							response.setContentType("text/plain;charset=UTF-8");
+							response.setContentType("text/plain;charset=UTF-8"); //html로 바꾸면 죄다 한줄로 나옴(인라인)
 							
 							PrintWriter out = response.getWriter();
 							
 							for(int i=1 ; i<=9 ; i++) {
 								for(int j=2 ; j<=9 ; j++) {
-									out.print(j+"*"+i+"="+j*i+"\t");
+									//out.print(j+"*"+i+"="+j*i+"\t");
+									out.printf("%d*%d=%2d ",j,i,j*i);
 								}
 								out.println();
 							}
