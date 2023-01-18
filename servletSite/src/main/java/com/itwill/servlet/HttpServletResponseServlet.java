c263401ecb0f12c47541da2a853fd7353396efcf
minyoung.c <devcmy136@gmail.com>
2023-01-18 10:53:45 +0900

    **

--- servletSite/src/main/java/com/itwill/servlet/HttpServletResponseServlet.java
+++ servletSite/src/main/java/com/itwill/servlet/HttpServletResponseServlet.java
@@ -31,10 +31,24 @@ public class HttpServletResponseServlet extends HttpServlet {
 			out.print("<h1>다시 요청하세요</h1><hr>");
 			out.print("<a href='04.HttpServletResponse.html'>04.HttpServletResponse.html</a>");
 			return;	
-		}
+		} //방어코드 -> 첫화면은 방어코드가 뜸 왜? 아무것도 없으니깐.
 		if (cmd.equals("1")) {
+			/*
+			 * <<정상응답>>
+			 * 	1.응답라인 상태코드 200
+			 *  2.응답헤더
+			 *  3.응답바디데이타(html)전송
+			 */
 			out.println("<h3>정상응답</h3><hr>");
 		}else if (cmd.equals("2")) {
+			/*
+			 * <<error응답>>
+			 *  1.응답라인 4xx,5xx
+			 *  2.응답헤더
+			 *  3.응답바디데이타없음(보낼수없음)
+			 */
+			response.sendError(403);
+			//response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); //500번
 			
 		}else if (cmd.equals("3")) {
 			
