d2c8914067a0d4a642b1f886e4154aa21435f56f
minyoung.c <devcmy136@gmail.com>
2023-01-30 11:18:29 +0900

    *

--- user_model1/src/main/java/com/itwill/user/UserService.java
+++ user_model1/src/main/java/com/itwill/user/UserService.java
@@ -1,10 +1,14 @@
 package com.itwill.user;
 /*
- * - 회원관리 업무(비즈니스로직,예외처리,트랜젝션,보안,로깅)을 수행하는 클래스
+ * - 회원관리 업무(비즈니스로직,예외처리,트랜젝션,보안,로깅,인증, 성능 등)을 수행하는 클래스
  * - 웹컴포넌트(서블릿,JSP)에서 직접접근(메쏘드호출)하는 클래스(객체)
  * - Dao를 이용해서 데이타베이스를 조작작업(CRUD)하는 클래스
  */
 public class UserService {
+	private UserDao userDao;
+	public UserService() throws Exception {
+		userDao = new UserDao();
+	}
 	
 	/*
 	 * 회원가입
--- user_model1/src/main/webapp/user_main.jsp
+++ user_model1/src/main/webapp/user_main.jsp
@@ -25,7 +25,6 @@
 		<div id="navigation">
 			<!-- include_common_left.jsp start-->
 			<jsp:include page="include_common_left.jsp" />
-
 			<!-- include_common_left.jsp end-->
 		</div>
 		<!-- navigation end-->
