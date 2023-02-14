257d49075700d99e32ae05ef64072f3ca724337a
minyoung.c <devcmy136@gmail.com>
2023-02-14 13:44:00 +0900

    **

---
+++ guest_model1.2/src/com/itwill/guest/controller/GuestWriteFormServlet.java
@@ -0,0 +1,47 @@
+package com.itwill.guest.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GuestWriteFormSevlet
+ */
+@WebServlet("/guest_write_form.do")
+public class GuestWriteFormServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String forwardPath = "";
+		forwardPath = "forward:/WEB-INF/views/guest_write_form.jsp";
+		
+		/*****************forward or redirect ***********************/
+		//위에는 forwardpath만 세우고 컨트롤러 아래에서 forward or redirect 로 보냄
+		/* 구분(default가 forwarding)
+		 * forward  --> forward:/WEB-INF/views/guest_xxx.jsp
+		 * redirect --> redirect:guest_xxx.do
+		 * redirect에 jsp있으면 무조건 404
+		 */
+		String[] pathArray = forwardPath.split(":");
+		String forwardOrRedirect = pathArray[0];
+		String path = pathArray[1];
+		
+		if(forwardOrRedirect.equals("redirect")) {
+			//redirect
+			response.sendRedirect(path);
+		}else {
+			//forwarding
+			RequestDispatcher rd = request.getRequestDispatcher(path);
+			rd.forward(request, response);
+			
+		}
+		/************************************************************/
+		
+	}
+
+}
