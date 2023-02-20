2de3d6d4fb1125cf9529038ba0dc7fb9d7bb2fc8
itwillPC <devcmy136@gmail.com>
2023-02-20 15:20:01 +0900

    //

--- user_model2.0/src/main/java/com/itwill/user/filter/LoginCheckFilter.java
+++ user_model2.0/src/main/java/com/itwill/user/filter/LoginCheckFilter.java
@@ -7,14 +7,24 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LoginCheckFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request; //(ServletRequest)가 상위라서, 캐스팅해줘야함
+		HttpServletResponse resp = (HttpServletResponse)response; //(ServletResponse)가 상위라서, 캐스팅해줘야함
 		
-		System.out.println("LoginCheckFilter-->");
+		//System.out.println("LoginCheckFilter-->");
+		
+		String sUserId = (String)req.getSession().getAttribute("sUserId");
+		if(sUserId==null) {
+			resp.sendRedirect("user_main.do");
+			return;
+		}
 	}
 
 }
